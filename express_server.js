77929ae5a5d9a5e2e6297e68b82673f46672a91e
Devon <devon.blake@live.com>
2019-08-06 23:17:19 +0000

    redirected short url, finished url shortnening pt.2

--- express_server.js
+++ express_server.js
@@ -35,6 +35,12 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + shortURL);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+   const shortURL = req.params.shortURL;
+   const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
