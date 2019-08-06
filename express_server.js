e59b001865fd255c271b5efab2729c9338401584
Devon <devon.blake@live.com>
2019-08-06 16:29:31 +0000

    added post request to body to console

--- express_server.js
+++ express_server.js
@@ -9,9 +9,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
