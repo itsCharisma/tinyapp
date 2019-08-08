87d30f246173ef7b84d0c1247ff0f12b5f13804e
Devon <devon.blake@live.com>
2019-08-08 02:06:15 +0000

    added login and logout if/else header and express_server

--- express_server.js
+++ express_server.js
@@ -53,6 +53,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls")
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username", req.body["username"]);
+  res.redirect("/urls")
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
